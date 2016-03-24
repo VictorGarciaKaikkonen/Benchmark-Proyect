a3d53aaa5c168241380f4f535348b8fee5670e70
Victor <v.garcia.kaikkonen@gmail.com>
2016-03-24 17:15:25 +0100

    First modification

--- Full_CPU_Test.java
+++ Full_CPU_Test.java
@@ -5,60 +5,71 @@ import java.util.Random;
 public class Full_CPU_Test extends Thread {
 
 	public void run() {
-
+		int sprints = 10;
 		double y = 0; //Just to hold the result of the formula evaluation 
-		double auxIterations = 1000000000 / 10;
+		double auxIterations = 1000000000/sprints; /*Splits, in as many sprints as the "sprints" variable is set to, the 
+		number of iterations so each core can print several times during the process*/ 
 
-		Random random = new Random();
-		double tiempoTotalIni = System.nanoTime();
-		for (int i = 0; i <= 10; i++) {
-
-			double iteracionTiempoIni = System.nanoTime();
+		Random random = new Random(); //Random number generator
+		double totalTimeStart = System.nanoTime(); //Total time starts ticking
+		
+		for (int i = 0; i <= 10; i++) { //Controls each sprint
+			double sprintTimeStart = System.nanoTime(); //Sprint time starts
 			for (int j = 0; j <= auxIterations; j++) {
 				int a = random.nextInt(900);
-				int b = random.nextInt(900);
+				int b = random.nextInt(900); 	//Ecuation's Random numbers generation
 				int c = random.nextInt(900);
 
-				y = (-b + Math.sqrt(4 * a * c)) / 2 * a;
+				y = (-b + Math.sqrt(4*a*c))/ 2*a; /*Calculates something with random numbers, in this case it is a 
+				second degree ecuation formula that can only throw Real numbers, not complex*/
 
 			}
-			double iteracionTiempoFin = System.nanoTime();
-			double tiempoIteracionNano = iteracionTiempoFin - iteracionTiempoIni;
-			double tiempoReguladoIteracion;
-			String unidadIteracion;
-			if (tiempoIteracionNano >= 1000000000) {
-				tiempoReguladoIteracion = tiempoIteracionNano / 1000000000;
-				unidadIteracion = "seconds";
-			} else if (tiempoIteracionNano >= 1000000 && tiempoIteracionNano <= 1000000000) {
-				tiempoReguladoIteracion = tiempoIteracionNano / 1000000;
-				unidadIteracion = "ms";
+			
+			double sprintTimeFinish = System.nanoTime(); //Sprint time finishes
+			double sprintTime = sprintTimeFinish - sprintTimeStart; //Total time spent on the sprint
+			double normalizedSprintTime; //To be used in the next method
+			/*The next method is a bit useless, as all modern computers will most likely get a unit of seconds, but tested
+			 * with an i5-3470 it gave about 4 seconds per sprint for each core with default values, so I don't doubt some
+			 * computers out there are going to get the "ms" mark*/
+			String sprintTimeUnit;
+			if (sprintTime >= 1000000000) {
+				normalizedSprintTime = sprintTime / 1000000000;
+				sprintTimeUnit = "seconds";
+			} else if (sprintTime >= 1000000 && sprintTime <= 1000000000) {
+				normalizedSprintTime = sprintTime / 1000000;
+				sprintTimeUnit = "ms";
 			} else {
-				tiempoReguladoIteracion = tiempoIteracionNano;
-				unidadIteracion = "ns";
+				normalizedSprintTime = sprintTime;
+				sprintTimeUnit = "ns";
 			}
-			System.out.println(auxIterations + " iterations finished in " + tiempoReguladoIteracion + " "
-					+ unidadIteracion + " (" + (tiempoIteracionNano) + " ns" + ")");
+			
+			System.out.println(auxIterations + " iterations finished in " + normalizedSprintTime + " " + sprintTimeUnit);
 		}
+		
+		/*Some nasty duplicated code that does the same useless thing as the method before, but this time with the total 
+		 * time. (To be deprecated in next versions)*/
+		double totalTimeFinish = System.nanoTime();
+		double totalTime = totalTimeFinish - totalTimeStart;
+		double normalizedTotalTime;
+		String unit;
+		if (totalTime >= 1000000000) {
+			normalizedTotalTime = totalTime / 1000000000;
+			unit = "seconds";
+		} else if (totalTime >= 1000000 && totalTime <= 1000000000) {
+			normalizedTotalTime = totalTime / 1000000;
 
-		double tiempoTotalFin = System.nanoTime();
-		double tiempoTotalNano = tiempoTotalFin - tiempoTotalIni;
-		double tiempoReguladoTotal;
-		String unidad;
-		if (tiempoTotalNano >= 1000000000) {
-			tiempoReguladoTotal = tiempoTotalNano / 1000000000;
-			unidad = "seconds";
-		} else if (tiempoTotalNano >= 1000000 && tiempoTotalNano <= 1000000000) {
-			tiempoReguladoTotal = tiempoTotalNano / 1000000;
-
-			unidad = "ms";
+			unit = "ms";
 		} else {
-			tiempoReguladoTotal = tiempoTotalNano;
-			unidad = "ns";
+			normalizedTotalTime = totalTime;
+			unit = "ns";
 		}
-		Main.sumarTiempos(tiempoReguladoTotal);
-		System.out.println("Total time spent on 1000000000 iterations was: " + tiempoReguladoTotal + " " + unidad + "("
-				+ (tiempoTotalNano) + "ns" + ")");
-		System.out.println("The last result in the evaluation of the expression was: " + y);
+		
+		Main.sumTimes(normalizedTotalTime);
+		System.out.println("Total time spent on "+ auxIterations*sprints +" iterations was: " + normalizedTotalTime + " " + unit+"."); 
+		//Prints total time
+		
+		/*System.out.println("The last result in the evaluation of the expression was: " + y); /*Ain't nobody got time for
+		knowing the last result, and they dont care either*/
 
 	}
 
--- Main.java
+++ Main.java
@@ -5,55 +5,61 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Date;
 
+/*		A brief and simple to-do list for the future
+ * 
+ * Implement a decent result giving method
+ * Implement a log file
+ * Implement the other classes
+ * Search of previous files in order to calculate a mean
+ * GUI
+ * */
+		
+
+
 public class Main {
 	static Date date = new Date();
-	static String nombreFichero;
-	static double tiempoTotal = 0;
-	static int veces = 0;
+	static String fileName;
+	static double totalTime = 0;
+	static int times = 0;
 	public static void main(String[] args) throws BenchmarkException {
 		
-		nombreFichero = date.toString();
-		int numCPUs = Runtime.getRuntime().availableProcessors();
-		System.out.println("Application running on "+ numCPUs +" cores.");
-		for(int i = 1; i <= numCPUs; i++){
+		fileName = date.toString();
+		int cores = Runtime.getRuntime().availableProcessors();
+		System.out.println("Application running on "+ cores +" cores.");
+		for(int i = 1; i <= cores; i++){
 			(new Full_CPU_Test()).start();
 		}
 	
-		//Implementar la exposición de resultados
-		//Implementar Escritura de fichero
-		//Implementar el resto de los algoritmos
-		//Búsqueda de fichero anterior
-		//Interfaz
 			}
 
 	
-	public static void sumarTiempos(double tiempo){
-			tiempoTotal += tiempo;
-			veces++;
+	public static void sumTimes(double time){
+			totalTime += time;
+			times++;
 			double totalIterations = 1000* (Runtime.getRuntime().availableProcessors());
-			if(veces == Runtime.getRuntime().availableProcessors()){
+			if(times == Runtime.getRuntime().availableProcessors()){
 			System.out.println("----------------------------------------------------------------------------------");
-			System.out.println("The sum of time used by all the CPUs is: "+ tiempoTotal+" seconds.");
-			System.out.println("The average time per core is: " + tiempoTotal/Runtime.getRuntime().availableProcessors()+ " seconds.");
-			System.out.println("----------------------------------------------------------------------------------");
+			//System.out.println("The sum of time used by all the CPUs is: "+ totalTime+" seconds.");//A bit of a nonsense
+			System.out.println("The average time per core is: " + totalTime/Runtime.getRuntime().availableProcessors()+ " seconds.");
+			//System.out.println("----------------------------------------------------------------------------------");
 			System.out.println("----------------------------------------------------------------------------------");
 			System.out.println("The total number of iterations is: " +totalIterations+" millions,");
-			System.out.println("which are done in "+tiempoTotal+"seconds");
-			System.out.println("Those values make a final result of: "+ (totalIterations)/(tiempoTotal/Runtime.getRuntime().availableProcessors()) + " million iterations per second");
+			System.out.println("which are done in "+totalTime+" seconds");
+			System.out.println("---> Those values make a final result of: "+ (totalIterations)/(totalTime/Runtime.getRuntime().availableProcessors()) + " million iterations per second <---");
 			System.out.println("----------------------------------------------------------------------------------");
 
 			}
 	}			
-	public static void imprimir(String datos){
+	
+	/*public static void imprimir(String datos){
 		
 		System.out.println(datos);
-		//escribirFichero(datos);
+		escribirFichero(datos);
 		
 		
-	}
+	}*/
 	
-
-
+	
 /*
 	private static void escribirFichero(String datos) throws FileNotFoundException {
 		
